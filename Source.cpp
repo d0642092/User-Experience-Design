8f47bf41e54d6284b452e45070fefd105ebdfaea
you0329 <39891156+you0329@users.noreply.github.com>
2019-11-23 22:58:46 +0800

    辨識手掌(還不能判斷數字)

--- Source.cpp
+++ Source.cpp
@@ -1,331 +1,129 @@
-#include <opencv2/highgui/highgui.hpp>  
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core.hpp>
-
-
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/photo.hpp>
-#include <time.h>
-#include <iostream>
+﻿#include "opencv2/opencv.hpp"  
 
 using namespace cv;
 using namespace std;
 
-Mat Sourceimage, Spilteimage, Rebuildimage, Dstimage;
-int rows, cols;
-int Roirows, Roicols;
-vector<Mat>arraryimage;
-void Randarrary(vector<Mat> &vectorMat);    //隨機排列子圖像序列函數
-static int vectornumber = 0;
-void OnMouseAction(int event, int x, int y, int flags, void *ustc);
-void video(); //攝影機
-void video2();
-Mat images(int x,int y);
-Mat creatImg(int x,int y,int z);
-int main(int argc, char** argv)
-{
-	video2(); //皮膚色
-	//video(); //藍色
+void skinExtract(const Mat &frame, Mat &skinArea);
 
-	//
-	//waitKey();
-	//images();
-	return 0;
-}
-Mat creatImg(int x,int y,int z) {
-	Mat img = Mat::zeros(x, y, z);
-	int i,j;
-	for ( j = 90;j <= y -160;j+=100) {
-		for (i = 110; i < x;i+=100) {
-			circle(img, Point(i, j), 50, Scalar(0, 0, 255), 2);
-		}
-		
-	}
-	return img;
-}
-Mat images(int x,int y) {
-	Sourceimage = imread("img.jpg");
-	//Mat backGround = Mat::zeros(600, 500, Sourceimage.type());
-	resize(Sourceimage, Sourceimage, Size(x, y));
-	//seamlessClone(Sourceimage, backGround, Sourceimage, Point(300,250), Sourceimage,NORMAL_CLONE);
-	imshow("Source image", Sourceimage);
-	rows = atoi("4");
-	cols = atoi("4");
-	Roirows = Sourceimage.rows / rows;
-	Roicols = Sourceimage.cols / cols;
-	Spilteimage = Mat::zeros(Sourceimage.rows, Sourceimage.cols, Sourceimage.type());
-	Dstimage = Mat::zeros(Sourceimage.rows, Sourceimage.cols, Sourceimage.type());
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
-			Mat SourceRoi = Sourceimage(Rect(j*Roicols, i*Roirows, Roicols - 1, Roirows - 1));
-			arraryimage.push_back(SourceRoi);
-		}
-	}
-	// 随機函數	
-	Randarrary(arraryimage);
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
-			Mat SpilterRoi = Spilteimage(Rect(j*Roicols, i*Roirows, Roicols - 1, Roirows - 1));
-			addWeighted(SpilterRoi, 0, arraryimage[vectornumber], 1, 0, SpilterRoi);
-			vectornumber++;
-			imshow("Splite image", Spilteimage);
-			waitKey(150);
-		}
-	}
-	
-	return Spilteimage;
-	
-}
-void Randarrary(vector<Mat>& vectorMat)
+int main(int argc, char* argv[])
 {
-	for (int i = 0; i < vectorMat.size(); i++)
-	{
-		srand(int(time(0)));
-		int a = rand() % (vectorMat.size() - i) + i;
-		swap(vectorMat[i], vectorMat[a]);
-	}
-}
+	Mat frame, skinArea;
+	VideoCapture capture;
 
-void OnMouseAction(int event, int x, int y, int flags, void *ustc)
-{
-	if (event == CV_EVENT_MOUSEMOVE)
+	capture.open(0);
+	if (!capture.isOpened())
 	{
-		Mat RoiSpilte, RoiSource;
-		int rows = (y / Roirows)*Roirows;
-		int clos = (x / Roicols)*Roicols;
-		RoiSpilte = Spilteimage(Rect(clos, rows, Roicols, Roirows));
-		imshow("Slice", RoiSpilte);
-		Mat image = Mat::zeros(Sourceimage.rows - Roirows, Sourceimage.cols - Roicols, CV_32FC1);
-		matchTemplate(Sourceimage, RoiSpilte, image, 1);
-		normalize(image, image, 0, 1, NORM_MINMAX);
-		double minV = 0;
-		double maxV = 0;
-		Point minP, maxP;
-		minMaxLoc(image, &minV, &maxV, &minP, &maxP);
-		Mat ROIDst = Dstimage(Rect(minP.x, minP.y, Roicols, Roirows));
-		addWeighted(ROIDst, 0, RoiSpilte, 1, 0, ROIDst, -1);
-		imshow("Jigsaw image", Dstimage);
+		cout << "No camera!\n" << endl;
+		return -1;
 	}
-}
-void video() //皮膚色
-{
-	VideoCapture cap(0);
-	Mat frame, frameHSV;
-	Mat Sourceimage = imread("kanahei.jpg");
-	vector< std::vector<cv::Point> > OriginalContours;
-	vector<Vec4i > hierarchy;
-	vector<vector<Point>> FinalContours;
-	vector<Point> hull;
-	int i, j;
-	int Width = cap.get(CV_CAP_PROP_FRAME_WIDTH), Height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
-	float fHeight = ((float)2 / 3 - (float)1 / 7) * Height;
-	while (1) {
 
-		cap >> frame;
+	while (1)
+	{
+		capture >> frame;
+		flip(frame, frame, 1);
 		if (frame.empty())
 			break;
 
-		flip(frame, frame, 1); //左右反轉
-		GaussianBlur(frame, frame, Size(7, 7), 2.0, 1.5); //高斯模糊
-		cvtColor(frame, frameHSV, CV_BGR2HSV); //通道轉換
-		Mat mask(frame.rows, frame.cols, CV_8UC1);
-		inRange(frameHSV, Scalar(5, 30, 30), Scalar(40, 170, 256), mask);
-		erode(mask, mask, Mat(5, 5, CV_8U), Point(-1, -1), 1); //去除多餘亮點
+		skinArea.create(frame.rows, frame.cols, CV_8UC1);
+		skinExtract(frame, skinArea);
+		Mat show_img;
+		frame.copyTo(show_img, skinArea);
 
-		//Mat grayscaleMat(frame.size(), CV_8U);
-		//cvtColor(frame, grayscaleMat, CV_BGR2GRAY);
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+		 
+		findContours(skinArea, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
 
-
-		OriginalContours.clear();
-		hierarchy.clear();
-		FinalContours.clear();
-		findContours(mask, OriginalContours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE); //找輪廓
-		// 去除多餘輪廓
-		for (i = 0; i < OriginalContours.size(); i++)
+		  
+		int index;
+		double area, maxArea(0);
+		for (int i = 0; i < contours.size(); i++)
 		{
-			if (fabs(contourArea(Mat(OriginalContours[i]))) > 12000)
+			area = contourArea(Mat(contours[i]));
+			if (area > maxArea)
 			{
-				FinalContours.push_back(OriginalContours[i]);
+				maxArea = area;
+				index = i;
 			}
 		}
-		//cv::drawContours(frame, FinalContours, -1, Scalar(0, 0, 255), 3); //畫輪廓
 
-		//找凸出輪廓
-		int hullcount;
-		int iNumOfContours = FinalContours.size();
-		Point top;
+		//drawContours(frame, contours, index, Scalar(0, 0, 255), 2, 8, hierarchy );  
 
+		Moments moment = moments(skinArea, true);
+		Point center(moment.m10 / moment.m00, moment.m01 / moment.m00);
+		circle(show_img, center, 8, Scalar(0, 0, 255), CV_FILLED);
 
-		for (j = 0; j < iNumOfContours; j++)
+		vector<Point> couPoint = contours[index];
+		vector<Point> fingerTips;
+		Point tmp;
+		int max(0), count(0), notice(0);
+		for (int i = 0; i < couPoint.size(); i++)
 		{
-			convexHull(Mat(FinalContours[j]), hull, true);
-			hullcount = (int)hull.size();
-			//
-			for (i = 0; i < hullcount - 1; i++)
+			tmp = couPoint[i];
+			int dist = (tmp.x - center.x) * (tmp.x - center.x) + (tmp.y - center.y) * (tmp.y - center.y);
+			if (dist > max)
 			{
-				line(frame, hull[i + 1], hull[i], Scalar(255, 0, 0), 2, CV_AA);
-				circle(frame, hull[(hullcount - 1) / 2], 4, Scalar(0, 255, 0), 1); //綠點
-
+				max = dist;
+				notice = i;
 			}
-			//line(frame, hull[hullcount - 1], hull[0], Scalar(255, 0, 0), 2, CV_AA);
-			//line(frame, top, top, Scalar(255, 0, 255), 18, CV_AA);
 
-			/*Point temp;
-
-
-			for (i = 0; i < (int)hull.size() - 1; i++) //找出綠點位置
+			if (dist != max)
 			{
-				for (j = 0; j < (int)hull.size() - 1; j++)
+				count++;
+				if (count > 60)
 				{
-					if (hull[i].x > hull[i + 1].x || hull[i].y > hull[i + 1].y)
+					count = 0;
+					max = 0;
+					bool flag = false;
+					if (center.y < couPoint[notice].y)
+						continue;
+					for (int j = 0; j < fingerTips.size(); j++)
 					{
-						temp.x = hull[i].x;
-						hull[i].x = hull[i + 1].x;
-						hull[i + 1].x = temp.x;
+						if (abs(couPoint[notice].x - fingerTips[j].x) < 20)
+						{
+							flag = true;
+							break;
+						}
 					}
+					if (flag) continue;
+					fingerTips.push_back(couPoint[notice]);
+					circle(show_img, couPoint[notice], 6, Scalar(0, 255, 0), CV_FILLED);
+					line(show_img, center, couPoint[notice], Scalar(255, 0, 0), 2);
 				}
-			}*/
-
-		}
-
-
-		imshow("Extracted Frame", frame);
-		imshow("Frame", frameHSV);
-
-		char c = (char)waitKey(25); //按esc結束程式
-		if (c == 27)
-			break;
-
-	}
-
-	for (i = 0; i < (int)hull.size(); i++) //找出綠點位置
-		cout << hull[i];
-
-}
-
-void video2()
-{
-	VideoCapture cap(0);
-	Mat img, imgHSV;
-	int i, j;
-
-	vector< std::vector<cv::Point> > OriginalContours;
-	vector<Vec4i > hierarchy;
-	vector<vector<Point>> FinalContours;
-	vector<Point> hull;
-	/*int iLowH = 100 / 2;
-	int iHighH = 120 / 2;
-	int iLowS = 50 * 255 / 100;
-	int iHighS = 70 * 255 / 100;
-	int iLowV = 40 * 255 / 100;
-	int iHighV = 50 * 255 / 100;*/
-
-
-	//藍色
-	int iLowH = 100;
-	int iHighH = 140;
-	int iLowS = 40;
-	int iHighS = 255;
-	int iLowV = 40;
-	int iHighV = 255;
-	Mat test = images(600, 500);
-	while (1) {
-
-		cap >> img;
-		
-		//Mat test = creatImg(img.rows, img.cols, img.type());
-		resize(img, img, Size(600,500));
-		imshow("t", test);
-		Mat backGround = Mat::zeros(img.rows, img.cols,img.type());
-		Mat out;
-		
-		Mat d;
-		if (img.empty())
-			break;
-
-		flip(img, img, 1); //左右反轉
-
-		cvtColor(img, imgHSV, COLOR_BGR2HSV);//转为HSV
-		Mat imgThresholded;
-		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
-		
-		Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
-		morphologyEx(imgThresholded, imgThresholded, MORPH_OPEN, element);
-		//闭操作 (连接一些连通域)
-		morphologyEx(imgThresholded, imgThresholded, MORPH_CLOSE, element);
-		namedWindow("Thresholded Image");
-
-		vector<vector<Point>> contours;
-		findContours(imgThresholded, contours, CV_RETR_EXTERNAL, CHAIN_APPROX_NONE); //找輪廓
-		vector<vector<Point>>::iterator cit = contours.begin();
-		while (cit != contours.end()) {
-			if (contourArea(*cit) < 300) { //過濾面積太小的區塊
-				cit = contours.erase(cit);
 			}
-			else cit++;
-		}
-		Mat dst = img.clone();
-		vector<vector<Point>> hull(contours.size());
-		cvtColor(imgThresholded, imgThresholded, CV_GRAY2BGR);
-		for (int i = 0; i < contours.size(); i++) { //畫出區塊
-			convexHull(contours[i], hull[i]);
-			drawContours(dst, hull, i, Scalar(0, 0, 255), 2);
 		}
 
-		backGround = img.clone();
-		//seamlessClone(test, backGround, NULL, Point(backGround.rows, backGround.cols), out, MONOCHROME_TRANSFER);
-		
-		absdiff(backGround, dst, d);
-		absdiff(test, d, out);
-		imshow("Thresholded Image", dst);
-		imshow("d", d );
-		imshow("finish", out);
-		//setMouseCallback("Splite image", OnMouseAction);
+		imshow("show_img", show_img);
 
-
-		//这里是自定义的求取形心函数，当然用连通域计算更好  
-		//Point center; 
-		//center = GetCenterPoint(imgThresholded);//获取二值化白色区域的形心     
-		//circle(img, center, 100, Scalar(0,0,255), 5, 8, 0);//绘制目标位置   
-		//imshow("Extracted Frame", img);
-
-		char c = (char)waitKey(25); //按esc結束程式
-		if (c == 27)
+		if (cvWaitKey(20) == 'q')
 			break;
 	}
+
+	return 0;
 }
 
-void hsvreg(Mat img) {
-	//Mat img = imread("蓝色笔筒.jpg", 1);
-	Mat imgHSV;
-	//蓝色笔筒颜色的HSV范围    
-	int iLowH = 100 / 2;
-	int iHighH = 120 / 2;
-	int iLowS = 50 * 255 / 100;
-	int iHighS = 70 * 255 / 100;
-	int iLowV = 40 * 255 / 100;
-	int iHighV = 50 * 255 / 100;
-	cvtColor(img, imgHSV, COLOR_BGR2HSV);//转为HSV
-	imwrite("hsv.jpg", imgHSV);
-	Mat imgThresholded;
-	inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
-	//Threshold the image
-	//开操作 (去除一些噪点)  如果二值化后图片干扰部分依然很多，增大下面的size    
-	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
-	morphologyEx(imgThresholded, imgThresholded, MORPH_OPEN, element);
-	//闭操作 (连接一些连通域)
-	morphologyEx(imgThresholded, imgThresholded, MORPH_CLOSE, element);
-	namedWindow("Thresholded Image", CV_WINDOW_NORMAL);
-	imshow("Thresholded Image", imgThresholded);
-	//这里是自定义的求取形心函数，当然用连通域计算更好  
-	//Point center; 
-	//center = GetCenterPoint(imgThresholded);//获取二值化白色区域的形心     
-	//circle(img, center, 100, Scalar(0,0,255), 5, 8, 0);//绘制目标位置   
-	imwrite("end.jpg", img);
-	waitKey(0);
+void skinExtract(const Mat &frame, Mat &skinArea)
+{
+	Mat YCbCr;
+	vector<Mat> planes;
+
+	cvtColor(frame, YCbCr, CV_RGB2YCrCb);
+	split(YCbCr, planes);
+
+	MatIterator_<uchar> it_Cb = planes[1].begin<uchar>(),
+		it_Cb_end = planes[1].end<uchar>();
+	MatIterator_<uchar> it_Cr = planes[2].begin<uchar>();
+	MatIterator_<uchar> it_skin = skinArea.begin<uchar>();
+
+	for (; it_Cb != it_Cb_end; ++it_Cr, ++it_Cb, ++it_skin)
+	{
+		if (138 <= *it_Cr &&  *it_Cr <= 170 && 100 <= *it_Cb &&  *it_Cb <= 127)
+			*it_skin = 255;
+		else
+			*it_skin = 0;
+	}
+
+	dilate(skinArea, skinArea, Mat(5, 5, CV_8UC1), Point(-1, -1));
+	erode(skinArea, skinArea, Mat(5, 5, CV_8UC1), Point(-1, -1));
 
 }
